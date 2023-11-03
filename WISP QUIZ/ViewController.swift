e5bb842479fa6846fe075362f2c3e281771b6d0b
Amit Sah <aksamitsah@gmail.com>
2023-11-03 23:49:47 +0530

    Initial Commit

---
+++ WISP QUIZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WISP QUIZ
+//
+//  Created by Amit Shah on 03/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
